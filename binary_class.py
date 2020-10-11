b75fbbd7195f66eeeabdc586c234fb26508310a0
erica <ericaraffa@virgilio.it>
2020-10-11 09:23:19 +0200

    Prova classificatore con testset

--- binary_class.py
+++ binary_class.py
@@ -1,12 +1,14 @@
+import utils as ut
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from keras.preprocessing.text import Tokenizer
 import sys
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
 from keras.models import Sequential
 from keras import layers
 from keras.preprocessing.sequence import pad_sequences
-import numpy as np
+
+
 
 #shackleton-s => target
 #dasovich-j => other
@@ -18,8 +20,8 @@ sentences_train = tf.keras.preprocessing.text_dataset_from_directory(
     class_names=None,
     batch_size=1,
     max_length=None,
-    shuffle=False,
-    seed=False,
+    shuffle=True,
+    seed=3210,
     validation_split=0.16, #Fraction of the training data to be used as validation data
     subset="training",
     follow_links=False,
@@ -32,8 +34,8 @@ sentences_val = tf.keras.preprocessing.text_dataset_from_directory(
     class_names=None,
     batch_size=1,
     max_length=None,
-    shuffle=False,
-    seed=False,
+    shuffle=True,
+    seed=3210,
     validation_split=0.16, #Fraction of the training data to be used as validation data
     subset="validation",
     follow_links=False,
@@ -46,8 +48,8 @@ sentences_test = tf.keras.preprocessing.text_dataset_from_directory(
     class_names=None,
     batch_size=1,
     max_length=None,
-    shuffle=True,
-    seed=False,
+    shuffle=False,
+    seed=None,
     validation_split=None, #Fraction of the training data to be used as validation data
     subset=None,
     follow_links=False,
@@ -65,7 +67,7 @@ y_test=[int(element[1][0]) for element in sentences_test.as_numpy_iterator()]
 
 
 #Tokenizer
-tokenizer = Tokenizer(num_words=5000)
+tokenizer = Tokenizer(num_words=16000)
 tokenizer.fit_on_texts(train)
 
 X_train = tokenizer.texts_to_sequences(train)
@@ -74,28 +76,32 @@ X_test = tokenizer.texts_to_sequences(test)
 
 vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
 
+
 #pad
-maxlen = 200
+maxlen = 100
 
 X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
 X_val = pad_sequences(X_val, padding='post', maxlen=maxlen)
 X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
 
-
-#Embedding layer
-embedding_dim = 50
+#Layers
+embedding_dim = 100
 
 model = Sequential()
 model.add(layers.Embedding(input_dim=vocab_size, 
                            output_dim=embedding_dim, 
-                           input_length=maxlen))
+                           input_length=maxlen,
+                           mask_zero=True))
 model.add(layers.Flatten())
-model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1000, activation='relu'))
+model.add(layers.Dense(20, activation='sigmoid'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam',
-              loss='binary_crossentropy',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 model.summary()
+
+
 
 #Training
 X_train = np.array(X_train)
@@ -106,13 +112,13 @@ X_test = np.array(X_test)
 y_test = np.array(y_test)
 
 history = model.fit(X_train, y_train,
-                    epochs=15,
-                    verbose=2,
+                    epochs=20,
+                    verbose=1,
                     validation_data=(X_val, y_val),
-                    batch_size=10)
+                    batch_size=32)
 loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
 print("Training Accuracy: {:.4f}".format(accuracy))
 loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
 print("Testing Accuracy:  {:.4f}".format(accuracy))
-#plot_history(history)
+ut.plot_history(history)
 print(history.history)
