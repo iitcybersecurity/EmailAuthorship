780d4fb266b11a07c7d266298521779f1371f72b
erica <ericaraffa@virgilio.it>
2020-10-22 12:32:40 +0200

    Last update dataset + models + logs

--- binary_new_dataset.py
+++ binary_new_dataset.py
@@ -1,5 +1,8 @@
-import utils as ut
+import json, os, math
+import csv
+from utils import *
 import tensorflow as tf
+from tensorboard.plugins import projector
 import sys
 import numpy as np
 from tensorflow import keras
@@ -8,89 +11,74 @@ from keras.models import Sequential
 from keras import layers
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-import json
 import random
-import math
+from time import time
+import datetime
 
-#shackleton-s => target
-#dasovich-j => other
+target_user = "shackleton-s"
+base_dir = "Dataset/RAW/enron_dataset/"
+log_dir = "Models/logs/first_mixed/" #+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-#change it with the appropriate path
-dataset_path = "/home/giacomo/CNR/EmailAuthorship/enron_dataset/"
-target_user = "dasovich-j"
-non_target_user = "shackleton-s"
-
-def read_data():
-	emails = []
-	labels = []
-	print(dataset_path + target_user + ".json")
-	with open(dataset_path + target_user + ".json") as f:
-		mails = json.load(f)
-	for mail in mails["sent"]:
-
-		emails.append(mail["body"])
-		labels.append(1)
-		
-	with open(dataset_path + non_target_user + ".json") as f:
-		mails = json.load(f)
-	for mail in mails["sent"]:
-		emails.append(mail["body"])
-		labels.append(0)
-	return emails, labels
+#Data for saving
+file_tokenizer = 'token_first_mixed.json'
+file_model = 'first_mod_mixed'
 
 #Read data from dataset
-dataset, labels = read_data()
+t_dataset, t_labels, nt_dataset, nt_labels = read_data(base_dir, target_user)
+t_len = len([elem for elem in t_labels])
+nt_len = len([elem for elem in nt_labels])
 
 #Shuffle dataset
-c = list(zip(dataset, labels))
-random.shuffle(c)
-dataset_shuffled, labels_shuffled = zip(*c)
+nt_dataset_shuffled, nt_labels_shuffled = shuffle_data(nt_dataset, nt_labels)
+dataset = t_dataset + nt_dataset[0:t_len]
+labels = t_labels + nt_labels[0:t_len]
+dataset_shuffled, labels_shuffled = shuffle_data(dataset, labels)
 
-#Get 10% of testing set. If you want you can take 80% training, 10% validation, 10% testing
-train = dataset_shuffled[0:math.floor(len(dataset_shuffled)*0.9)]
-test = dataset_shuffled[math.floor(len(dataset_shuffled)*0.9)+1:]
-y_train = labels_shuffled[0:math.floor(len(dataset_shuffled)*0.9)]
-y_test = labels_shuffled[math.floor(len(dataset_shuffled)*0.9)+1:]
+#Get 80% training, 10% validation, 10% testing
+train, val, test = split_dataset(dataset_shuffled, 0.8, 0.1, 0.1)
+y_train, y_val, y_test = split_dataset(labels_shuffled, 0.8, 0.1, 0.1)
 
 print("Training samples: " + str(len(y_train)))
+print("Val samples: " + str(len(y_val)))
 print("Testing samples: " + str(len(y_test)))
 
 #Tokenizer
 tokenizer = Tokenizer(num_words=16000)
 tokenizer.fit_on_texts(train)
+tokenizer.fit_on_texts(val)
+tokenizer.fit_on_texts(test)
+
+#salve tokenizer
+save_tokenizer_in(file_tokenizer , tokenizer)
 
 X_train = tokenizer.texts_to_sequences(train)
-#X_val = tokenizer.texts_to_sequences(val)
+X_val = tokenizer.texts_to_sequences(val)
 X_test = tokenizer.texts_to_sequences(test)
-
 vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+word_index = tokenizer.word_index
+subwords = word_index
 
+#with open('word_index.txt', 'w') as f:
+#   print(word_index, file=f)
+
+#logs metadata
+# Save Labels separately on a line-by-line manner.
+with open(os.path.join(log_dir, 'metadata.tsv'), 'w') as f:
+	f.write("Word\tLabel\n")
+	for key, value in subwords.items():
+		f.write("{}\t{}\n".format(str(key), value))
+  # Fill in the rest of the labels with "unknown"
+	#for unknown in range(1, vocab_size - len(subwords)):
+	#	f.write("unknown #{}\n".format(unknown))
 
 #pad
 maxlen = 50
-
 X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
-#X_val = pad_sequences(X_val, padding='post', maxlen=maxlen)
+X_val = pad_sequences(X_val, padding='post', maxlen=maxlen)
 X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
-
 #Layers
-embedding_dim = 100
+embedding_dim = 50
 '''
-model = Sequential()
-model.add(layers.Embedding(input_dim=vocab_size, 
-                           output_dim=embedding_dim, 
-                           input_length=maxlen,
-                           mask_zero=True))
-model.add(layers.Flatten())
-model.add(layers.Dense(100, activation='relu'))
-model.add(layers.Dense(20, activation='sigmoid'))
-model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-model.summary()
-'''
-
 model = Sequential()
 model.add(layers.Embedding(vocab_size, embedding_dim, input_length=maxlen))
 model.add(layers.Conv1D(128, 5, activation='relu'))
@@ -99,16 +87,59 @@ model.add(layers.Dense(10, activation='relu'))
 model.add(layers.Dense(2, activation='sigmoid'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
+'''
+model = Sequential()
+model.add(layers.Embedding(vocab_size, 
+                           embedding_dim, 
+                           input_length=maxlen,     
+                           trainable=True,
+                           mask_zero=True))
+model.add(layers.SpatialDropout1D(0.2))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(2, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+#keras.utils.plot_model(model, show_shapes=True)
 
+#logs
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 #Training
 y_train = to_categorical(y_train)
+y_val = to_categorical(y_val)
 y_test = to_categorical(y_test)
 
-history = model.fit(X_train, y_train, epochs=200, verbose=1, validation_data=(X_test, y_test), batch_size=64)
-loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+history = model.fit(X_train, y_train, 
+                    epochs=15, 
+                    verbose=1, 
+                    validation_data=(X_val, y_val), 
+					callbacks=[tensorboard_callback],
+                    batch_size=64)
+loss, accuracy = model.evaluate(X_val, y_val, verbose=False)
 print("Training Accuracy: {:.4f}".format(accuracy))
 loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
 print("Testing Accuracy:  {:.4f}".format(accuracy))
-ut.plot_history(history)
+model.save('Models/' + file_model)
+
+plot_history(history)
 print(history.history)
+
+# Save weights
+weights = tf.Variable(model.layers[0].get_weights()[0][1:])
+
+# Create a checkpoint from embedding
+checkpoint = tf.train.Checkpoint(embedding=weights)
+checkpoint.save(os.path.join(log_dir, "embedding.ckpt"))
+
+# Set up config
+config = projector.ProjectorConfig()
+embedding = config.embeddings.add()
+embedding.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
+embedding.metadata_path = 'metadata.tsv'
+projector.visualize_embeddings(log_dir, config)
+
+# %tensorboard --logdir Models/logs/first_mixed 
+# => Projector => Load "metadata.tsv"
